ec152f0852dccdec9e16b147f872ff93d9ee4208
2EZ4BLINK <blinkcode24@gmail.com>
2026-06-13 11:54:01 +0800

    update: count even numbers

--- countEvenNumbers.ts
+++ countEvenNumbers.ts
@@ -1,16 +1,18 @@
 const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const countEven = (numbers: number[]): number => {
-  const finalOuput: number[] = [];
+  const finalOutput: number[] = [];
 
   numbers.forEach((num) => {
-    if (num % 2 === 0) finalOuput.push(num);
+    if (num % 2 === 0) finalOutput.push(num);
   });
 
-  console.log("finalOuput: ", finalOuput);
-  return finalOuput.length;
+  console.log("finalOutput: ", finalOutput);
+  return finalOutput.length;
 };
 
 const data = countEven(values);
 
 console.log("data: ", data);
+
+// Other way use filter
