846af4a0d7769b3fb758c0078c095dfccab15f30
2EZ4BLINK <blinkcode24@gmail.com>
2026-08-10 18:31:09 +0800

    feat: add a palindrom brain teaser

---
+++ palindromSTring.ts
@@ -0,0 +1,12 @@
+const isPalindrome = (word: string) => {
+  let reversed: string[] = [];
+
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversed.push(word[i]);
+  }
+
+  return word === reversed.join("");
+};
+
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("hello"));
