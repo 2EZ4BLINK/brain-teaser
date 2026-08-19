603961f584d5987b922c66872605a0a0d9e8d68a
2EZ4BLINK <blinkcode24@gmail.com>
2026-08-20 03:22:12 +0800

    feat: added removeDuplicates

--- prep.ts
+++ prep.ts
@@ -4,11 +4,24 @@ const numbers = [5, 2, 8, 1, 9, 3];
 const returnLargest = (numbers: number[]): number[] => {
   const largerThanFour = numbers.filter((i) => i > 4);
 
-  console.log("largerThanFour: ", largerThanFour);
-
   return largerThanFour;
 };
 
-console.log("numbers: ", numbers);
-
 returnLargest(numbers);
+
+// Return the numbers without duplicates.
+const numbersTwo = [1, 2, 2, 3, 4, 4, 5, 5, 5];
+
+const removeDuplicates = (numbers: number[]): number[] => {
+  let noDuplicateNumbers: number[] = [];
+
+  numbers.forEach((item, i) => {
+    if (noDuplicateNumbers.includes(item)) return;
+
+    noDuplicateNumbers.push(item);
+  });
+
+  return noDuplicateNumbers;
+};
+
+removeDuplicates(numbersTwo);
