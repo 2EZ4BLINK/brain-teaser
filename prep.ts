4e8fef63f0518abc35e77499ba6496d0646c429f
2EZ4BLINK <blinkcode24@gmail.com>
2026-08-20 03:37:23 +0800

    feat: added findLargestNumber

--- prep.ts
+++ prep.ts
@@ -15,7 +15,7 @@ const numbersTwo = [1, 2, 2, 3, 4, 4, 5, 5, 5];
 const removeDuplicates = (numbers: number[]): number[] => {
   let noDuplicateNumbers: number[] = [];
 
-  numbers.forEach((item, i) => {
+  numbers.forEach((item) => {
     if (noDuplicateNumbers.includes(item)) return;
 
     noDuplicateNumbers.push(item);
@@ -25,3 +25,18 @@ const removeDuplicates = (numbers: number[]): number[] => {
 };
 
 removeDuplicates(numbersTwo);
+
+// Find the largest number.
+const numbersThree = [10, 5, 8, 20, 3, 15];
+
+const findLargestNumber = (numbersThree: number[]): number => {
+  let largestNumber = numbersThree[0];
+
+  for (let i = 0; i < numbersThree.length; i++) {
+    if (largestNumber < numbersThree[i]) largestNumber = numbersThree[i];
+  }
+
+  return largestNumber;
+};
+
+findLargestNumber(numbersThree);
